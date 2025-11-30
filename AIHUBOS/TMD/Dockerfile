541a2a0636b50c7ceae7a347786774292ac60c26
letrongphu18 <trphu1810@gmail.com>
2025-11-30 18:05:25 +0700

    add docker

---
+++ AIHUBOS/TMD/Dockerfile
@@ -0,0 +1,16 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:9.0-preview AS build
+WORKDIR /src
+
+COPY TMD/TMD/TMD.csproj ./TMD.csproj
+RUN dotnet restore "./TMD.csproj"
+
+COPY TMD/. ./TMD/
+WORKDIR /src/TMD
+
+RUN dotnet publish -c Release -o /app/publish
+
+FROM mcr.microsoft.com/dotnet/aspnet:9.0-preview AS final
+WORKDIR /app
+
+COPY --from=build /app/publish ./
+ENTRYPOINT ["dotnet", "TMD.dll"]
--- AIHUBOS/TMD/obj/Debug/net9.0/AIHUBOS.AssemblyInfo.cs
+++ AIHUBOS/TMD/obj/Debug/net9.0/AIHUBOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHUBOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a868a0c1ee41697dce95ee6495bb24d6df9de1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2cfa027ee467f2ae1167339090419acb3ddf92")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHUBOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHUBOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
